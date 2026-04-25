a176f6af7067e29995c982072dae46e03e81c81f
진성현 <96581463+jsh1102@users.noreply.github.com>
2026-04-25 14:35:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const words = ["향상하는", "열정적인", "배가고픈", "도전적인", "창의적인", "헌신적인", "ISFP"];
+  let wordIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+
+  const typedSpan = document.getElementById("typed-word");
+
+  function typeEffect() {
+    const currentWord = words[wordIndex];
+    
+    if (isDeleting) {
+      typedSpan.textContent = currentWord.substring(0, charIndex--);
+    } else {
+      typedSpan.textContent = currentWord.substring(0, charIndex++);
+    }
+
+    let typingSpeed = isDeleting ? 80 : 130;
+    if (!isDeleting && charIndex > currentWord.length) {
+      isDeleting = true;
+      typingSpeed = 1000; 
+    }
+
+    if (isDeleting && charIndex === 0) {
+      isDeleting = false;
+      wordIndex = (wordIndex + 1) % words.length;
+      typingSpeed = 500;
+    }
+
+    setTimeout(typeEffect, typingSpeed);
+  }
+
+  document.addEventListener("DOMContentLoaded", typeEffect);
