23cde39f59a479be3be1c21249d42f4e7b66a5d7
진성현 <96581463+jsh1102@users.noreply.github.com>
2026-04-25 16:24:08 +0900

    dd

--- script.js
+++ script.js
@@ -32,11 +32,11 @@ const words = ["향상하는", "열정적인", "배가고픈", "도전적인", "
   document.addEventListener("DOMContentLoaded", typeEffect);
 
   const container = document.querySelector('.container');
-const boxes = document.querySelectorAll('.start-box, .mid-box, .last-box');
+const boxes = document.querySelectorAll('.start-box, .mid-box, .last-box, .real-last-box');
 
 const options = {
     root: container,
-    threshold: 0.5 // 섹션이 50% 이상 보일 때 색상 변경
+    threshold: 0.5 
 };
 
 const observer = new IntersectionObserver((entries) => {
